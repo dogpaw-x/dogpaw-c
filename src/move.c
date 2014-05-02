ed1a5bf70620782be328b703b3db128c6d0ebb15
mingang.he <dustgle@gmail.com>
2014-05-02 09:49:47 +0800

    士移动位置初始化

--- src/move.c
+++ src/move.c
@@ -1,5 +1,6 @@
 #include "move.h"
 
+/* 红帅可移动位置 */
 struct bit_board R_KING_MOVEABLE = {
 	{
 		0, 0, 0,
@@ -11,10 +12,35 @@ struct bit_board R_KING_MOVEABLE = {
 	},
 };
 
+/* 黑将可移动位置 */
 struct bit_board B_KING_MOVEABLE = {
 	{
 		0, 0, 0,
 		1<<6|1<<7|1<<8, 1<<6|1<<7|1<<8, 1<<6|1<<7|1<<8,
+		0, 0,
+		0, 0,
+		0, 0, 0,
+		0, 0, 0
+	},
+};
+
+/* 红士可移动位置 */
+struct bit_board R_ADVISOR_MOVEABLE = {
+	{
+		0, 0, 0,
+		0, 0, 0,
+		0, 0,
+		0, 0,
+		1<<6|1<<8, 1<<7, 1<<6|1<<8,
+		0, 0, 0
+	},
+};
+
+/* 黑士可移动位置 */
+struct bit_board B_ADVISOR_MOVEABLE = {
+	{
+		0, 0, 0,
+		1<<6|1<<8, 1<<7, 1<<6|1<<8,
 		0, 0,
 		0, 0,
 		0, 0, 0,
